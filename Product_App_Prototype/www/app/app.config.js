91976cca72cbf668fbca2ae509b91a6c92908cb1
Gaurav b <Gaurav b>
2017-12-26 19:38:57 +0530

    Add project files.

---
+++ Product_App_Prototype/www/app/app.config.js
@@ -0,0 +1,10 @@
+﻿(function () {
+    "use strict";
+
+    angular.module("pap").config(["$logProvider", "$compileProvider", papConfig]);
+
+    function papConfig($logProvider, $compileProvider) {
+        $logProvider.debugEnabled(true);
+        $compileProvider.debugInfoEnabled(true);
+    }
+})();
---
+++ Product_App_Prototype/www/app/app.run.js
@@ -0,0 +1,9 @@
+﻿(function () {
+    "use strict";
+
+    angular.module("pap").run(["nativeService", runBlock]);
+
+    function runBlock(nativeService) {
+        nativeService.initListners();
+    }
+})();
