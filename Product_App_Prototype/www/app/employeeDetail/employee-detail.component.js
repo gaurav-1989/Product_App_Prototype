c307342d7293d753f42f2cc3bc19683cd940742b
Manikandan Sankar <mani.merit2008@gmail.com>
2017-12-27 18:01:13 +0530

    Changes done

---
+++ Product_App_Prototype/www/app/employeeDetail/employee-detail.component.js
@@ -0,0 +1,14 @@
+﻿(function () {
+	debugger
+	angular.module('employee.detail').component('employeeDetail', {
+		templateUrl: 'app/employeeDetail/employee-detail-template.html',
+		controller: function employeeDetailController() {
+			debugger
+								this.user = [
+				{					Firstname: 'Manikandan',
+									LastName: 'Sankar'
+				}
+			];
+		}
+	});
+})();
